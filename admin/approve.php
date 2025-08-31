71708ab929c281a2d9322957314a02301be1c0c4
Sagarro3137 <shuboroy2004@gmail.com>
2025-08-31 16:19:07 +0530

    Update approve.php

--- admin/approve.php
+++ admin/approve.php
@@ -1,1 +1,18 @@
+<?php
+$file = "../uploads/".$_GET['file'];
+$coins = intval($_GET['coins']);
+$user = explode("_", $_GET['file'])[0];
 
+$data = json_decode(file_get_contents("../users.json"), true);
+
+foreach ($data as &$u) {
+    if ($u['username'] == $user) {
+        $u['coins'] += $coins;
+    }
+}
+file_put_contents("../users.json", json_encode($data, JSON_PRETTY_PRINT));
+
+unlink($file);
+
+header("Location: admin.php");
+?>
